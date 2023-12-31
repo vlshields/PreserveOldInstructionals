6b211e6fb5336183880d3385952d9a339ab67f19
Vince Shields <vince.shields913@gmail.com>
2023-12-30 18:54:46 -0800

    Scraping url was getting wonky, trying to only grab whats inside the div tag

--- scapetitles.py
+++ scapetitles.py
@@ -15,34 +15,42 @@ included in the repo called "convertvobfiles.ps1"""
 
 import requests
 from bs4 import BeautifulSoup
-
-url = "https://jiujitsux.com/courses/keenans-lapel-encyclopedia/"
-
-# Set headers to simulate a web browser request
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-}
-
-# Send a GET request with headers
-response = requests.get(url, headers=headers)
-
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Parse the HTML content of the page
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Find all elements and extract the text
-    all_text = soup.get_text(separator='\n', strip=True)
-    titles = []
-    for lines in all_text:
-        titles.append(lines)
-
+def get_titles():
+    url = "https://jiujitsux.com/courses/keenans-lapel-encyclopedia/"
+    
+    # Set headers to simulate a web browser request
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    }
+    
+    # Send a GET request with headers
+    response = requests.get(url, headers=headers)
+    
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the page
+        soup = BeautifulSoup(response.content, 'html.parser')
+    
+        # Find all elements and extract the text
+        all_text = soup.get_text(separator='\n', strip=True)
+        return all_text
+    
+all_text = get_titles()
+    
+print(all_text[2307:]) 
+#print(all_text.find("Chapter 1"))    
+"""
+def write_titles(all_text):
+    
     # Print the extracted text
     text_file_path = 'output1.txt'
-# Write the extracted text to a text file
+    
+    # Write the extracted text to a text file
     with open(text_file_path, 'w', encoding='utf-8') as text_file:
+        
         text_file.write(all_text)
 
-    print(f"Text written to {text_file_path}")
-else:
-    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+        print(f"Text written to {text_file_path}")
+    else:
+        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+"""
