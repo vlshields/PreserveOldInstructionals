82ee2c64df85abefcc385de5a14dd4941e8795ca
Vince Shields <vince.shields913@gmail.com>
2023-12-30 22:24:59 -0800

    adding loops to finally rename files

--- scapetitles.py
+++ scapetitles.py
@@ -5,7 +5,7 @@ Spyder Editor
 Copyright Vincent Shields
 """
 
-import requests
+
 from bs4 import BeautifulSoup
 import os
 from chapter1 import html_doc
@@ -15,25 +15,30 @@ from getfiles import get_files_list
 The were converted to .mkv files with the powershell script 
 included in the repo called "convertvobfiles.ps1"""
 
-import requests
-from bs4 import BeautifulSoup
 
 def get_chapter_1():
-    soup = BeautifulSoup(html_doc,'html.parser')
-
     
+    soup = BeautifulSoup(html_doc,'html.parser')
     titles = [text for text in soup.stripped_strings]
     return titles
 
 def make_chapter_1_folder(titles):
     os.makedirs("C:\\Users\\Vince\\Desktop\\WormGuard\\{titles[0]}")
     
-def add_titles_with_duration(titles):
-    torename = get_files_list()
+def separate_duration_from_titles(titles):
+    
     duration = list()
+    titles.pop[0]
     for i in titles:
         if len(i) < 8:
             duration.append(i)
             del titles[i]
     return duration,titles
+
+def add_chap_and_length(duration,titles,oldfiles):
+    duration,titles = separate_duration_from_titles(titles)
+    oldfiles = get_files_list()
+    for title,duration,old in zip(titles,duration,oldfiles):
+        os.rename(old, title + duration + '.mkv')
+    
     
