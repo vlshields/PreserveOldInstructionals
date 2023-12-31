db1354949e562d302f2c185400bb399061a0324e
Vince Shields <vince.shields913@gmail.com>
2023-12-30 21:47:43 -0800

    Added a new file to return list of videos that need to be renamed. also separated the timestamps from the video titles

--- scapetitles.py
+++ scapetitles.py
@@ -9,6 +9,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from chapter1 import html_doc
+from getfiles import get_files_list
 
 """Worm guard instructions came as .vob files in dvd format. 
 The were converted to .mkv files with the powershell script 
@@ -17,7 +18,22 @@ included in the repo called "convertvobfiles.ps1"""
 import requests
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(html_doc,'html.parser')
-print(soup.prettify())
+def get_chapter_1():
+    soup = BeautifulSoup(html_doc,'html.parser')
 
-# [text for text in soup.stripped_strings]
+    
+    titles = [text for text in soup.stripped_strings]
+    return titles
+
+def make_chapter_1_folder(titles):
+    os.makedirs("C:\\Users\\Vince\\Desktop\\WormGuard\\{titles[0]}")
+    
+def add_titles_with_duration(titles):
+    torename = get_files_list()
+    duration = list()
+    for i in titles:
+        if len(i) < 8:
+            duration.append(i)
+            del titles[i]
+    return duration,titles
+    
