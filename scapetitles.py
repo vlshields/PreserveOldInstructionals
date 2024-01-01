416ca8b3ab9470baa4b338f4ab3723c9ce4c9fe7
Vince Shields <vince.shields913@gmail.com>
2023-12-31 16:47:41 -0800

    Made several changes

--- scapetitles.py
+++ scapetitles.py
@@ -8,37 +8,57 @@ Copyright Vincent Shields
 
 from bs4 import BeautifulSoup
 import os
-from chapter1 import html_doc
-from getfiles import get_files_list
+from chapter1 import chapter2
+from glob import glob
+
 
 """Worm guard instructions came as .vob files in dvd format. 
 The were converted to .mkv files with the powershell script 
 included in the repo called "convertvobfiles.ps1"""
 
+class ScrapeLapelGuard:
+    
+    def __init__(self,chapter,html):
+        
+        self.chapter = chapter
+        self.html = html
+        
+    def get_titles(self):
+        
+        soup = BeautifulSoup(self.html,'html.parser')
+        titles = [text for text in soup.stripped_strings if "Chapter" not in text]
+        titles = [i for i in titles if len(i)>6]
+        titles = [i.replace("50/50","FiftyFifty") for i in titles]
+        titles = [i.replace(":"," - ") for i in titles]
+    
+    
+        return titles
 
-def get_chapter_1():
+    def make_chapter_folder(self):
+        new_folder_path = "C:\\Users\\Vince\\Desktop\\WormGuard\\"+self.chapter
+        if not os.path.exists(new_folder_path):
+            os.makedirs(new_folder_path)
+        return new_folder_path
     
-    soup = BeautifulSoup(html_doc,'html.parser')
-    titles = [text for text in soup.stripped_strings]
-    return titles
+    def get_duration(self):
+        soup = BeautifulSoup(self.html,'html.parser')
+        titles = [text for text in soup.stripped_strings if "Chapter" not in text]
+        duration = [i for i in titles if len(i) < 6]
+        return duration
 
-def make_chapter_1_folder(titles):
-    os.makedirs("C:\\Users\\Vince\\Desktop\\WormGuard\\{titles[0]}")
+def getfiles(filepath):
+    return list(glob(filepath))
     
-def separate_duration_from_titles(titles):
+if __name__=='__main__':
     
-    duration = list()
-    titles.pop[0]
-    for i in titles:
-        if len(i) < 8:
-            duration.append(i)
-            del titles[i]
-    return duration,titles
-
-def add_chap_and_length(duration,titles,oldfiles):
-    duration,titles = separate_duration_from_titles(titles)
-    oldfiles = get_files_list()
-    for title,duration,old in zip(titles,duration,oldfiles):
-        os.rename(old, title + duration + '.mkv')
+    disk2 = ScrapeLapelGuard("chapter2", chapter2)
+    titles = disk2.get_titles()
+    duration = disk2.get_duration()
+    folder = disk2.make_chapter_folder()
     
+    old = getfiles("C:\\Users\\Vince\\Desktop\\WormGuard\\*.mkv")
     
+    for oldfile,newfile,time in zip(old,titles,duration):
+        time = time.replace(':', '-')
+        
+        os.rename(oldfile,folder+'\\'+str(newfile)+"_"+str(time)+".mkv")
